8005454874316ba27080993145f4eefd599a556f
yssefnoam <2020noam@gmail.com>
2022-02-10 16:26:07 +0100

    write vector function definition

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -4,74 +4,144 @@
 
 namespace ft
 {
-    template <class T, class Alloc = std::allocator<T> >
-    class vector
-    {
-    private:
+	template <class T, class Alloc = std::allocator<T> >
+	class vector
+	{
+	private:
+	public:
+		/* Member functions */
 
-    public:
+		// constructor
+		explicit vector(const allocator_type &alloc = allocator_type());
+		explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type());
+		template <class InputIterator>
+		vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type());
+		vector(const vector &x);
 
-    /* Member functions */
+		// destructor
+		~vector();
 
-    // constructor
-    // destructor
-    // operator=
+		// operator=
+		vector &operator=(const vector &x);
+
+		/* Iterators */
+
+		// begin
+		iterator begin();
+		const_iterator begin() const;
+
+		// end
+		iterator end();
+		const_iterator end() const;
+
+		// rbegin
+		reverse_iterator rbegin();
+		const_reverse_iterator rbegin() const;
+
+		// rend
+		reverse_iterator rend();
+		const_reverse_iterator rend() const;
+
+		/* Capacity */
+
+		// size
+		size_type size() const;
+
+		// max_size
+		size_type max_size() const;
+
+		// resize
+		void resize (size_type n, value_type val = value_type());
+
+		// capacity
+		size_type capacity() const;
+
+		// empty
+		bool empty() const;
+
+		// reserve
+		void reserve (size_type n);
+
+		/* Element access */
+
+		// operator[]
+		reference operator[] (size_type n);
+		const_reference operator[](size_type n) const;
+
+		// at
+		reference at (size_type n);
+		const_reference at(size_type n) const;
+
+		// front
+		reference front();
+		const_reference front() const;
+
+		// back
+		reference back();
+		const_reference back() const;
+
+		/* Modifiers */
+
+		// assign
+		template <class InputIterator>
+		void assign(InputIterator first, InputIterator last);
+		void assign (size_type n, const value_type& val);
+
+		// push_back
+		void push_back (const value_type& val);
+
+		// pop_back
+		void pop_back();
+
+		// insert
+		iterator insert (iterator position, const value_type& val);
+		void insert (iterator position, size_type n, const value_type& val);
+		template <class InputIterator>
+		void insert(iterator position, InputIterator first, InputIterator last);
+
+		// erase
+		iterator erase (iterator position);
+		iterator erase(iterator first, iterator last);
+
+		// swap
+		void swap (vector& x);
+
+		// clear
+		void clear();
 
 
-    /* Iterators */
+		/* Allocator */
 
-    // begin
-    // end
-    // rbegin
-    // rend
+		// get_allocator
+		allocator_type get_allocator() const;
 
+		/* Non-member function overloads */
 
-    /* Capacity */
+		// relational operators
+		template <class T, class Alloc>
+		bool operator==(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+		template <class T, class Alloc>
+		bool operator!=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+		template <class T, class Alloc>
+		bool operator<(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+		template <class T, class Alloc>
+		bool operator<=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+		template <class T, class Alloc>
+		bool operator>(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+		template <class T, class Alloc>
+		bool operator>=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+		// swap
+		template <class T, class Alloc>
+		void swap(vector<T, Alloc> &x, vector<T, Alloc> &y);
 
-    // size
-    // max_size
-    // resize
-    // capacity
-    // empty
-    // reserve
+		/* Template specializations */
 
-
-    /* Element access */
-
-    // operator[]
-    // at
-    // front
-    // back
-
-
-    /* Modifiers */
-
-    // assign
-    // push_back
-    // pop_back
-    // insert
-    // erase
-    // swap
-    // clear
-
-
-    /* Allocator */
-
-    // get_allocator
-
-
-    /* Non-member function overloads */
-
-    // relational operators
-    // swap
-
-
-    /* Template specializations */
-
-    // vector<bool>
-
-
-    };
+		// vector<bool>
+		template <class T, class Alloc = allocator<T> >
+		class vector; // generic template
+		template <class Alloc>
+		class vector<bool, Alloc>; // bool specialization
+	};
 }
 
 #endif
