ce68489e7ed9572fe913d0edd21e894f7ae659d0
yssefnoam <2020noam@gmail.com>
2022-02-28 16:11:53 +0100

    test

--- Vector/iterators_traits.hpp
+++ Vector/iterators_traits.hpp
@@ -1,4 +1,19 @@
 #ifndef _ITERATOR_TRAITS_HPP_
 #define _ITERATOR_TRAITS_HPP_
 
+template <class Iterator>
+class iterator_traits
+{
+};
+
+template <class T>
+class iterator_traits<T *>
+{
+};
+
+template <class T>
+class iterator_traits<const T *>
+{
+};
+
 #endif
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,4 +1,52 @@
-// iterator_traits example
-#include <iostream> // std::cout
-#include <iterator> // std::iterator_traits
-#include <typeinfo> // typeid
+// C++ program to find the type of iterator
+#include <iostream>
+#include <vector>
+
+using namespace std;
+template <class T>
+
+// function to return the iterator type
+string get_iterator_type(T it)
+{
+	// if the iterator category of (it) matches input
+	if (typeid(typename iterator_traits<T>::iterator_category)
+		== typeid(input_iterator_tag))
+		return "Input";
+
+	// if the iterator category of (it) matches output
+	else if (typeid(typename iterator_traits<T>::iterator_category)
+			== typeid(output_iterator_tag))
+		return "Output";
+
+	// if the iterator category of (it) matches forward
+	else if (typeid(typename iterator_traits<T>::iterator_category)
+			== typeid(forward_iterator_tag))
+		return "Forward";
+
+	// if the iterator category of (it) matches bidirectional
+	else if (typeid(typename iterator_traits<T>::iterator_category)
+			== typeid(bidirectional_iterator_tag))
+		return "Bidirectional";
+
+	// if the iterator category of (it) matches random_access
+	else if (typeid(typename iterator_traits<T>::iterator_category)
+			== typeid(random_access_iterator_tag))
+		return "Random_Access";
+
+	// if the iterator category of (it)
+	// does not match any of the above
+	return "Missing";
+}
+
+// Driver code
+int main()
+{
+	vector<int> v;
+
+	// iterator that will be checked
+	vector<int>::reverse_iterator it = v.rbegin();
+
+	cout << get_iterator_type(it) << " Iterator\n";
+
+	return 0;
+}
