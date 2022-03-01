bc88a92d5b048dbc2831cc5f2419c7f2f0ff18a7
yssefnoam <2020noam@gmail.com>
2022-03-01 19:39:54 +0100

    commit

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <list>
+#include "vector.hpp"
 
 int main()
 {
@@ -10,18 +10,9 @@ int main()
 
 	int a[5] = {1,2,3,4,5};
 	ft::iterator<int> it(a);
-	// std::vector<int> vec(a, a+5);
-	// std::vector<int>::iterator it = vec.begin();
-	// it++;
-	// std::cout << *it << std::endl;
-	// std::vector<int>::iterator it2(it);
-	// std::cout << *it2 << std::endl;
-
-	// --it;
-	// if (it == it2)
-	// 	std::cout << "equal" << std::endl;
-	
-	// std::cout << it.base() << std::endl;
-	// std::cout << it2.base() << std::endl;
+	ft::iterator<int> it2(a);
+	it++;
+	if (it != it2)
+		std::cout << "equal" << std::endl;
 
 }
