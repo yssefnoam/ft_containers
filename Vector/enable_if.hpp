0bfaa4b7ed95c2c0c46576510abedf78636257da
yssefnoam <2020noam@gmail.com>
2022-03-11 19:42:33 +0100

    try to figure out the enable_if and is_integral

---
+++ Vector/enable_if.hpp
@@ -0,0 +1,9 @@
+#ifndef _ENABLE_IF_HPP_
+#define _ENABLE_IF_HPP_
+
+template <bool Cond, class T = void>
+struct enable_if
+{
+};
+
+#endif
---
+++ Vector/is_integral.hpp
@@ -0,0 +1,9 @@
+#ifndef _IS_INTEGRAL_HPP_
+#define _IS_INTEGRAL_HPP_
+
+template <class T>
+struct is_integral
+{
+};
+
+#endif
