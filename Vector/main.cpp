fb3d9e83708fc139dd90bb376e553df0147e736a
yssefnoam <2020noam@gmail.com>
2022-03-12 15:38:12 +0100

    a

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -2,23 +2,21 @@
 #include <vector>
 #include "vector.hpp"
 #define EL std::endl;
-#define NS std
+#define NS ft
 
 int main()
 {
   NS::vector<int> vec;
-  vec.reserve(4);
+  vec.reserve(3);
   std::cout << vec.capacity() << EL
   std::cout << vec.size() << EL
   NS::vector<int>::iterator it;
   for(it = vec.begin(); it < vec.end(); it++)
     std::cout << *it << EL
-  vec.resize(3);
+  vec.push_back(10);
   std::cout << "-----" << EL
   std::cout << vec.capacity() << EL
   std::cout << vec.size() << EL
   for(it = vec.begin(); it < vec.end(); it++)
     std::cout << *it << EL
-
-
 }
