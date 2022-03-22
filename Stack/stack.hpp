aff021e494bc765cc8fb3bfb82d0c64b223de6f6
Youssef Noam <ynoam@e1r12p3.1337.ma>
2022-03-22 22:56:04 +0100

    stack

--- Stack/stack.hpp
+++ Stack/stack.hpp
@@ -8,19 +8,27 @@ namespace ft
     template <class T, class Container = ft::vector<T> >
     class stack
 	{
+    public:
+        typedef T           value_type;
+        typedef Container   container_type;
+        typedef size_t      size_type;
+
+    private:
+        container_type _container;
+    public:
         // constructor
-        explicit stack (const container_type& ctnr = container_type());
+        explicit stack(const container_type &ctnr = container_type()) : _container(ctnr){};
 
-        bool empty() const;
+        bool empty() const {return _container.empty()};
 
-        size_type size() const;
+        size_type size() const{return _container.size()};
 
-        value_type& top();
-        const value_type& top() const;
+        value_type &top(){return _container.back()};
+        const value_type &top() const {return _container.back()};
 
-        void push(const value_type &val);
+        void push(const value_type &val){return _container.push_back()};
 
-        void pop();
+        void pop(){return _container.pop_back()};
     };
     template <class T, class Container>
     bool operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs);
