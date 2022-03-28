e17fbb34c81f025d2cab624b203a76afccd23e5a
Youssef Noam <ynoam@e1r8p2.1337.ma>
2022-03-28 21:52:21 +0000

    pair

---
+++ Map/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "pair.hpp"
+
+int main(void)
+{
+    ft::pair<std::string, double> product1;                   // default constructor
+    ft::pair<std::string, double> product2("tomatoes", 2.30); // value init
+    ft::pair<std::string, double> product3(product2);         // copy constructor
+
+    // product1 = std::make_pair(std::string("lightbulbs"), 0.99); // using make_pair (move)
+
+    product2.first = "shoes"; // the type of first is string
+    product2.second = 39.90;  // the type of second is double
+    product3 = product2;
+
+    // std::cout << "The price of " << product1.first << " is $" << product1.second << '\n';
+    std::cout << "The price of " << product2.first << " is $" << product2.second << '\n';
+    std::cout << "The price of " << product3.first << " is $" << product3.second << '\n';
+    return 0;
+}
--- Map/pair.hpp
+++ Map/pair.hpp
@@ -1,9 +1,35 @@
+namespace ft
+{
 #ifndef _PAIR_HPP
 #define _PAIR_HPP
 
 template <class T1, class T2>
 struct pair
 {
+    typedef T1 first_type;
+    typedef T2 second_type;
+
+    first_type		first;
+    second_type		second;
+
+	pair() : first(), second() {}
+
+    template <class U, class V>
+    pair(const pair<U, V> &pr)
+    {
+        first = pr.first;
+        second = pr.second;
+    }
+
+    pair(const first_type &a, const second_type &b) : first(a), second(b) {}
+    pair &operator=(const pair &pr)
+    {
+        this->~pair();
+        first = pr.first;
+        second = pr.second;
+        return *this;
+    }
 };
 
 #endif
+};
