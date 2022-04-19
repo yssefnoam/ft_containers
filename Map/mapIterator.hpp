1ae50a3f9b5449bb60cf73e672f4b6051fe91e1a
Youssef Noam <ynoam@e1r12p1.1337.ma>
2022-04-19 20:26:35 +0000

    aa

--- Map/mapIterator.hpp
+++ Map/mapIterator.hpp
@@ -4,42 +4,46 @@
 #include "iterator_traits.hpp"
 #include "avl.hpp"
 
-template <class Key, class T>
+template <class Iter, class Key, class T>
 class myIter
 {
 public:
-    typedef typename ft::iterator_traits<Iter>::reference reference;
-    typedef typename ft::iterator_traits<Iter>::pointer pointer;
-    typedef typename ft::iterator_traits<Iter>::value_type value_type;
-    typedef typename ft::iterator_traits<Iter>::difference_type difference_type;
-    typedef typename ft::iterator_traits<Iter>::iterator_category iterator_category;
+    typedef typename ft::iterator_traits<Iter>::reference           reference;
+    typedef typename ft::iterator_traits<Iter>::pointer             pointer;
+    typedef typename ft::iterator_traits<Iter>::value_type          value_type;
+    typedef typename ft::iterator_traits<Iter>::difference_type     difference_type;
+    typedef typename ft::iterator_traits<Iter>::iterator_category   iterator_category;
+    typedef ft::Tree<const Key, T>::node_pointer                    node_pointer;
 
 private:
-    pointer &_avl;
-    ft::Tree<const Key, T> node_pointer = *_current;
+    pointer         _avl;
+    node_pointer    _current;
 
+    pointer avl() const { return _avl; }
+    node_pointer current() const { return _current; }
 public:
-    myIter(ft::Tree<const Key, T> &base) : _avl(base) {
-        _current = base.smallestNode(base.root());
-    }
+    myIter(pointer *base)
+    : _avl(base)
+    , _current = _avl->smallestNode(_avl->root());
+    {}
 
     myIter()
-    : _avl()
-    {
-    }
+    : _avl(NULL)
+    , _current(NULL)
+    {}
 
     template <class T>
     myIter(const myIter<T> &copy)
     : _avl(copy.avl())
-    {
-    }
+    , _current(copy.current())
+    {}
 
     template <class T>
     myIter &operator=(const myIter<T> &copy)
-    {
-        _base = copy.base();
-        return *this;
-    }
+    : _avl(copy.avl())
+    , _current(copy.current())
+    { return *this; }
+
     myIter operator--()
     {
         _base--;
@@ -67,16 +71,15 @@ public:
     }
 
     template <class T>
-    bool operator==(const myIter<T> &other) { return (_base == other.base()); }
+    bool operator==(const myIter<T> &other) { return (_avl == other.avl() && _current == other.current()); }
 
     template <class T>
     bool operator!=(const myIter<T> &other) { return !operator==(other); }
 
-    reference operator*() const { return *_base; }
+    reference operator*() const { return *(operator->()); }
 
-    pointer operator->() { return _base; }
+    pointer operator->() { return _current->content; }
 
-    pointer base() const { return _base; }
 };
 
 #endif
